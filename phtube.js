8234dc05dfd72f3f16f2e6bb13b6cffedbad6c85
SaidulGit <saidulhero4820@gmail.com>
2023-09-02 16:11:24 -0600

    news create

--- phtube.js
+++ phtube.js
@@ -7,10 +7,9 @@ const tabContainer = document.getElementById("tab-container");
 data.data.forEach((items) => {
      const div = document.createElement("div");
     div.innerHTML = `
-    <a class="tab bg-gray-200 text-base ">${items.category} </a>
+    <a onclick="loadNews('${items.category_id}')" class="tab bg-gray-200 text-base ">${items.category} </a>
     `;
     tabContainer.appendChild(div);
-
 
 
 
@@ -18,8 +17,51 @@ data.data.forEach((items) => {
 
 
 
-console.log(data.data);
-
 };
+
+const loadNews = async (categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+
+    const data = response.json();
+
+   let allData =[];
+    await data.then((red)=>{
+       allData = red.data
+    } );
+    const cardContainer = document.getElementById("card-container");
+    console.log(allData);
+    allData.forEach((news) => {
+
+    const div = document.createElement("div");
+    div.innerHTML= `
+    <div class="card bg-base-100 shadow-xl">
+    <figure><img src=${news.thumbnail} /></figure>
+    <div class="card-body">
+       <div class="flex flex-auto gap-2">
+          <div class="w-9 rounded-full" >
+          <img src=${news.authors[0].profile_picture
+          } />
+          </div>
+          <div>
+          <h2 class="card-title">
+          ${news.title}
+          </h2>
+          </div>
+       
+       </div>
+       <div class="flex flex-auto gap-2">
+         <p>${news.authors[0].profile_name}</p>
+                   <div class="card-actions justify-start">
+                      <div class="badge badge-outline">Products</div>
+                 </div>
+        </div>
+      <p>${news.others.views}</p>
+    </div>
+  </div>
+    `;
+    cardContainer.appendChild(div);
+    console.log();
+   });
+}
 
 handleCategory();
